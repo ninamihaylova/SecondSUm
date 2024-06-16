f6140cedd490c0a71d2fada1ab73156326210efe
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 16:38:50 +0300

    Initial commit

---
+++ SecondSUm/Program.cs
@@ -0,0 +1,20 @@
+﻿class SecondSUm {
+    public static void Main (String[]args) {
+        
+        int timeFirst=int.Parse(Console.ReadLine()); 
+        int timeSecond=int.Parse (Console.ReadLine ());
+        int timeThird=int.Parse(Console. ReadLine());
+        int totalTime=timeFirst+timeSecond+timeThird;
+      
+        int minutes=totalTime/ 60;
+        int seconds=totalTime%60;
+        
+        if (seconds<10) {
+            Console.WriteLine ($"{minutes}:0{seconds}");
+        }else{
+            Console.WriteLine ($"{minutes}:{seconds}");
+
+        }
+
+    }
+}
